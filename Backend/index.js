e29be79bc87a300a05e18315081ea735e617a76c
mukeshkumar098 <mukeshkemar567@gmail.com>
2025-07-12 23:14:03 +0530

    check

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 app.use(cors({
- origin: ("http://localhost:5173/","https://realestate-delta-ecru.vercel.app"),
+ origin: "https://realestate-delta-ecru.vercel.app",
   credentials: true
 }));
 app.get("/",(req,res)=>{
